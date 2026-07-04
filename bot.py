591756b356397db9d80f4ab64f2eeb71b4b0d605
w6cr7p7tzw-tech <w6cr7p7tzw@privaterelay.appleid.com>
2026-07-04 15:46:51 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import requests
+
+TELEGRAM_TOKEN = 8845119426:AAEd8BW50q6yKz4K-8tz4D7ABi-lFlRoo5Y
+CHAT_ID = 417209770
+
+URL = "https://api.metals.live/v1/spot/silver"
+
+LOW = 70
+HIGH = 80
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+def get_price():
+    r = requests.get(URL).json()
+    return float(r[0]["price"])
+
+price = get_price()
+
+if price <= LOW:
+    send(f"🔵 LOW ALERT: {price}")
+elif price >= HIGH:
+    send(f"🔴 HIGH ALERT: {price}")
+else:
+    print("OK:", price)
